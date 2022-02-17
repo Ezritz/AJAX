f04f3e0352c25a6096bf9e3ba1a8847146022d27
ezritz <lidia.edith.escoto@gmail.com>
2022-02-16 19:11:11 -0600

    Create request with fetch

--- ajax.js
+++ ajax.js
@@ -36,4 +36,38 @@
     xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 
     xhr.send();
-})()
+})();
+
+// API Fetch
+(() => {
+    const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();  
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+        console.log(res);
+        // validamos si es que hay un error y este pueda mostrarse
+        return res.ok ? res.json(): Promise.reject(res);
+    })
+    .then((json) => {
+        // convertimos body a texto, se mostrara en arreglo de objetos
+        console.log(json)
+        json.forEach((elem) => {
+            // creamos elementos del dom li y en estos mostraremos los elementos del json
+            const $li = document.createElement("li");
+            $li.innerHTML = `${elem.name} -- ${elem.email} -- ${elem.phone}`;
+            $fragment.appendChild($li);
+        });
+
+        $fetch.appendChild($fragment)
+       
+    })
+    .catch((err) => {
+        console.log(err);
+        let message = err.statusText || "Ocurrio un error";
+        $fetch.innerHTML = `Error ${err.status}: ${message}`;
+        
+    }).finally(()=>{
+       console.log('Esto se ejecutara siempre, sin importar el resultado de fetch');
+    })
+})();
